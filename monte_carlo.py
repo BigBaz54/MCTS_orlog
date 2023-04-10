45eadf61c6943880f8d7e05c1d9415295a226897
BigBaz <louisvincent@famillecapelli.fr>
2023-04-11 00:25:35 +0200

    [+] monte carlo skeleton

---
+++ monte_carlo.py
@@ -0,0 +1,22 @@
+import time
+
+
+class MonteCarlo():
+    def __init__(self, game, max_time_seconds=1):
+        self.game = game
+        self.states = []
+        self.max_time_seconds = max_time_seconds
+        # dictionary of states and their win rates
+        self.played = {}
+        self.won = {}
+
+    def update(self, state):
+        self.states.append(state)
+
+    def get_best_move(self):
+        begin = time.time()
+        while time.time() - begin < self.max_time_seconds:
+            self.run_simulation()
+
+    def run_simulation(self):
+        pass
