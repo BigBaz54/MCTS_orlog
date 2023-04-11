b3e65edcf98450ade7b520f59b12410b4911b83a
BigBaz <louisvincent@famillecapelli.fr>
2023-04-11 19:21:51 +0200

    [-] remove prints

--- monte_carlo.py
+++ monte_carlo.py
@@ -31,7 +31,6 @@ class MonteCarlo():
         while time.time() - begin < self.max_time_seconds:
             self.run_simulation()
             games += 1
-            # print("Game number", games)
 
         # list of (move, state) tuples
         # for all legal moves and their resulting game states
@@ -42,7 +41,6 @@ class MonteCarlo():
             game_copy.do_move(move)
             state = game_copy.get_state()
             moves_states.append((move, state))
-            # print("move treated:", move, "state reached", state)
 
         # pick the move with the highest winrate, if there are multiple, pick randomly
         winrates_moves = [(self.won.get((player, state), 0) / self.played.get((player, state), 1), move) for move, state in moves_states]
@@ -83,10 +81,6 @@ class MonteCarlo():
             moves_states = []
             for move in legal_moves:
                 game_copy = game.copy()
-                # print('player', game_copy.current_player)
-                # print('turns played', game_copy.players[game_copy.current_player].turns_played)
-                # print('rolled dice', game_copy.players[game_copy.current_player].rolled_dice)
-                # print('move', move, 'game', game_copy.get_state())
                 game_copy.do_move(move)
                 state = game_copy.get_state()
                 moves_states.append((move, state))
@@ -106,11 +100,9 @@ class MonteCarlo():
                                              if ((won[(player, move_state[1])] / played[(player, move_state[1])]) +self.exploration_param * sqrt(log_total / played[(player, move_state[1])])) 
                                              == max_value]
                 move, state = random.choice(greatest_UCB_moves_states)
-                # print("UCB", move, state, "\n\n\n")
             else:
                 # otherwise, just make an arbitrary decision
                 move, state = random.choice(moves_states)
-                # print("rand", move, state, "\n\n\n")
 
             # player refers to the player who made the move leading to the state
             if expand and (player, state) not in played:
@@ -138,7 +130,7 @@ if __name__ == "__main__":
 
     # play a game between two monte carlo bots
     g = game.Game()
-    bot = MonteCarlo(g, 1.4, 30)
+    bot = MonteCarlo(g, max_time_seconds=5, exploration_param=1.4)
     while not g.is_over():
         g.players[g.current_player].roll_dice()
         
@@ -147,6 +139,6 @@ if __name__ == "__main__":
         print("Playing", move)
         g.do_move(move)
         g.end_turn()
-        print("Player 1:", g.players[0].hp)
-        print("Player 2:", g.players[1].hp)
+        print("Player 0:", g.players[0].hp)
+        print("Player 1:", g.players[1].hp)
         print()
