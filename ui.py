e56ac5a9829e1a2f0a01a7fa1a10cdde72772b5b
BigBaz <louisvincent@famillecapelli.fr>
2023-04-17 22:05:45 +0200

    [+] alternating turn between player and bot working

--- ui.py
+++ ui.py
@@ -1,3 +1,4 @@
+import random
 from customtkinter import *
 import tkinter as tk
 from PIL import Image
@@ -36,7 +37,6 @@ class SettingsView(CTkFrame):
         self.exp_label.grid(row=4, column=1, columnspan=2, sticky="e", pady=5, padx=(60, 5))
         self.exp_entry = CTkEntry(self, placeholder_text="1.4", width=40, justify="center")
         self.exp_entry.grid(row=4, column=3, columnspan=2, padx=5)
-        print(self.exp_entry.get())
 
         self.games_label = CTkLabel(self, text="Max games simulated", font=('Helvetica', 15))
         self.games_label.grid(row=5, column=1, columnspan=2, sticky="e", pady=5, padx=5)
@@ -58,7 +58,7 @@ class SettingsView(CTkFrame):
         games = self.games_entry.get()
         time = self.time_entry.get()
         self.destroy()
-        new_game = game.Game(max_hp=int(hp or 15), max_rerolls=int(reroll or 3))
+        new_game = game.Game(max_hp=int(hp or 15), max_rerolls=int(reroll or 3), current_player=random.randint(0, 1))
         bot = monte_carlo.MonteCarlo(new_game, exploration_param=float(exp or 1.4), max_simulations=int(games or 1000), max_time_seconds=float(time or 5))
         self.app.show_game(new_game, bot)
 
@@ -137,7 +137,7 @@ class GameView(CTkFrame):
         self.new_game_button.place(x=5, y=5, anchor="nw")
 
         # turn label
-        self.turn_player_var = tk.StringVar(value="Turn 1: Bot")
+        self.turn_player_var = tk.StringVar(value="Turn 1: "+("Player" if self.game.current_player == 0 else "Bot"))
         self.turn_label = CTkLabel(self.left_frame, textvariable=self.turn_player_var, font=('Helvetica', 25))
         self.turn_label.grid(row=1, column=0, columnspan=2, pady=(50, 20))
 
@@ -154,7 +154,7 @@ class GameView(CTkFrame):
         CTkLabel(self.rolled_dice_frame, text="", height=170).grid(row=1, column=0, sticky="nsew")
 
         # confirm/roll button
-        self.confirm_roll_button = CTkButton(self.left_frame, text="Roll dice", width=40, font=('Helvetica', 30), command=self.roll_dice)
+        self.confirm_roll_button = CTkButton(self.left_frame, text="Start", width=40, font=('Helvetica', 30), command=self.start)
         self.confirm_roll_button.grid(row=3, column=0, columnspan=2, pady=35)
 
         # player's info
@@ -163,7 +163,7 @@ class GameView(CTkFrame):
         self.player_frame.grid_columnconfigure(0, weight=1)
         self.player_frame.grid_columnconfigure(7, weight=1)
 
-        self.player_hp_var = tk.StringVar(value="HP: "+str(self.game.players[0].hp))
+        self.player_hp_var = tk.StringVar(value="Player HP: "+str(self.game.players[0].hp))
         self.player_hp_label = CTkLabel(self.player_frame, textvariable=self.player_hp_var, font=('Helvetica', 18), width=250)
         self.player_hp_label.grid(row=0, column=0, columnspan=8, sticky="nsew", pady=5, padx=5)
 
@@ -182,7 +182,7 @@ class GameView(CTkFrame):
         # label to set heigth of the frame
         CTkLabel(self.bot_frame, text="", height=100).grid(row=1, column=0, sticky="nsew")
 
-        self.bot_hp_var = tk.StringVar(value="HP: "+str(self.game.players[1].hp))
+        self.bot_hp_var = tk.StringVar(value="Bot HP: "+str(self.game.players[1].hp))
         self.bot_hp_label = CTkLabel(self.bot_frame, textvariable=self.bot_hp_var, font=('Helvetica', 18), width=250)
         self.bot_hp_label.grid(row=0, column=0, columnspan=8, sticky="nsew", pady=5, padx=5)
 
@@ -191,10 +191,13 @@ class GameView(CTkFrame):
         CTkButton(self.bot_frame, text="", image=self.little_arrow_img, width=30, height=30, fg_color="#eeeee4", hover_color="#eeeee4").grid(row=1, column=3, pady=5, padx=5)
         CTkButton(self.bot_frame, text="", image=self.little_shield_img, width=30, height=30, fg_color="#eeeee4", hover_color="#eeeee4").grid(row=1, column=4, pady=5, padx=5)
 
-
-
-
         
+    def start(self):
+        if (self.game.current_player==1):
+            self.do_bot_turn()
+        else:
+            self.confirm_roll_button.configure(text="Roll dice")
+            self.confirm_roll_button.configure(command=self.roll_dice)
 
     def switch_view(self):
         self.destroy()
@@ -204,8 +207,12 @@ class GameView(CTkFrame):
         self.game.roll_dice()
         self.rolled_dice = self.game.players[self.game.current_player].rolled_dice
         self.update_rolled_dice()
-        self.confirm_roll_button.configure(text="Confirm")
-        self.confirm_roll_button.configure(command=self.confirm)
+        if (self.game.current_player==0):
+            self.confirm_roll_button.configure(text="Confirm")
+            self.confirm_roll_button.configure(command=self.confirm)
+        else:
+            self.confirm_roll_button.configure(text="Ok")
+            self.confirm_roll_button.configure(command=self.confirm)
     
     def update_rolled_dice(self):
         for e in (self.rolled_dice_buttons):
@@ -237,16 +244,32 @@ class GameView(CTkFrame):
 
     def end_turn(self):
         self.game.end_turn()
-        self.player_hp_var.set("HP: " + str(self.game.players[0].hp))
-        self.bot_hp_var.set("HP: " + str(self.game.players[1].hp))
-        self.confirm_roll_button.configure(text="Roll Dice")
-        self.confirm_roll_button.configure(command=self.roll_dice)
+        self.player_hp_var.set("Player HP: " + str(self.game.players[0].hp))
+        self.bot_hp_var.set("Bot HP: " + str(self.game.players[1].hp))
         self.turn_player_var.set(f"Turn {self.game.players[self.game.current_player].turns_played+1}: {'Player' if (self.game.current_player==0) else 'Bot'}")
         self.rolled_dice = []
         self.selected_dice = []
         self.update_rolled_dice()
+        if (self.game.current_player==1 or self.game.players[0].turns_played==2):
+            self.do_bot_turn()
+        else:
+            self.confirm_roll_button.configure(text="Roll Dice")
+            self.confirm_roll_button.configure(command=self.roll_dice)
 
+    def do_bot_turn(self):
+        self.roll_dice()
+        # disable all buttons during bot turn
+        for e in (self.rolled_dice_buttons):
+            e.configure(state="disabled")
+        bot_move = self.bot.get_best_move()
 
+        # display the move selected by the bot
+        for e in (bot_move):
+            for but in (self.rolled_dice_buttons):
+                if (self.selected_dice[self.rolled_dice_buttons.index(but)]==0):
+                    if (but.cget("image")==self.arrow_img and e=="Arrow" or but.cget("image")==self.axe_img and e=="Axe" or but.cget("image")==self.shield_img and e=="Shield" or but.cget("image")==self.helmet_img and e=="Helmet"):
+                        self.toggle_dice(self.rolled_dice_buttons.index(but))
+                        break
 
     def toggle_dice(self, i):
         if (self.selected_dice[i]==1):
